b6a8b7b100a52333bbddc82ddd06b564c6cc056b
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-19 14:57:17 -0400

    added signup url

--- hoodip4/urls.py
+++ hoodip4/urls.py
@@ -7,9 +7,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.welcome, name = 'welcome'),
-    # url(r'^signup/$', views.signup, name='signup'),
-    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     views.activate, name='activate'),
+    url(r'^$', views.home, name='home'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
     # url(r'^profile/$', views.profile, name='profile'),
     # url(r'^post_project/$', views.post_project, name='post_project'),
     # url(r'^edit_profile/$', views.edit_profile, name='edit_profile'),
--- hoodip4/views.py
+++ hoodip4/views.py
@@ -37,6 +37,22 @@ def signup(request):
         form = SignupForm()
     return render(request, 'signup.html', {'form': form})
 
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')    
+
 def welcome(request):
 
     return render(request, 'index.html')
