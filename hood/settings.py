567d559f20e9d7216fa3c295dea09f669ce96054
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-19 11:33:47 -0400

    installed bootstrap3

--- hood/settings.py
+++ hood/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hoodip4.apps.Hoodip4Config',
-    # 'bootstrap3',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
