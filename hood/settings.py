7794076b2498c5d4c15617f7b1df2d7c23343ccf
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-19 11:28:19 -0400

    added base.html

--- hood/settings.py
+++ hood/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hoodip4.apps.Hoodip4Config',
+    # 'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
