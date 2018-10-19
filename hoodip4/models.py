2339d16998fe74ba91ba939969d5a7befda2f9d9
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-19 16:00:38 -0400

    changed font for the login

--- hoodip4/models.py
+++ hoodip4/models.py
@@ -5,6 +5,14 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
@@ -18,14 +26,6 @@ class Profile(models.Model):
     def delete_profile(self):
         self.delete()
 
-    @receiver(post_save, sender=User)
-    def create_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_profile(sender, instance, **kwargs):
-        instance.profile.save()
 
     def __str__(self):
         return self.user_name
