04566f0baecb1d85283f65a41016c88b6d444a59
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-22 13:47:12 -0400

    added url

--- hoodip4/urls.py
+++ hoodip4/urls.py
@@ -11,12 +11,11 @@ urlpatterns = [
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^profile/$', views.profile, name='profile'),
-    # url(r'^post_project/$', views.post_project, name='post_project'),
+    url(r'^posts/$', views.posts, name='posts'),
     url(r'^edit_profile/$', views.edit_profile, name='edit_profile'),
-    # url(r'^search/$', views.search_results, name = 'search_results'),
-    # url(r'^review/(?P<project_id>\d+)$', views.review, name = 'review'),
-
-    # url(r'^api/proj/$', views.ProjectList.as_view()),
+    url(r'^business/$', views.business, name='business'),
+    url(r'^neighborhood/$', views.neighborhood, name='neighborhood'),
+    url(r'^search/$', views.search_results, name = 'search_results'),
 ]
 
 if settings.DEBUG:
