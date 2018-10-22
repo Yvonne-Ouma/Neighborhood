9a1ab76a294c175f1d4dc71591f98fcdcef6f682
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-22 13:46:27 -0400

    made changes

--- hoodip4/admin.py
+++ hoodip4/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Profile,Posts,Business,Location,Neighborhood
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Posts)
+admin.site.register(Business)
+admin.site.register(Location)
+admin.site.register(Neighborhood)
