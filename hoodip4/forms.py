9ac2172e76c07af02c25989218b447ee1b7df7f1
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-22 13:46:32 -0400

    made changes

--- hoodip4/forms.py
+++ hoodip4/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile,Posts,Business,Neighborhood
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
@@ -12,4 +12,19 @@ class SignupForm(UserCreationForm):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['user_name', 'bio', 'profile_picture']       
+        fields = ['user_name', 'bio', 'profile_picture','neighborhood','location']  
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Posts
+        exclude =['user']
+             
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user']    
+
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        exclude = ['user']                  
